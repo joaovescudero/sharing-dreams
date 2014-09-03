fb83f87c61e2803c51e464e8b4a48496f089005a
Guilherme Vieira Rizzo <guivr2011@gmail.com>
2014-09-03 18:19:10 -0300

    Create art.php

---
+++ art.php
@@ -0,0 +1,80 @@
+<html>
+    
+    <head>
+        <meta charset='UTF-8'>
+        <title>Sharing Dreams</title>
+        <link rel="stylesheet" href="css/index.css">
+        <link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
+    </head>
+    
+    <body>
+        <!-- FACEBOOK and pinterest !-->
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.0";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+        <!-- OK. !-->
+
+        <div class="top">
+            <div class="logo">
+                <a href='/'><img src="img/logo.png"></a>
+            </div>
+            <ul>
+                <li>About</li>
+                <li><a href="/join" id="menu">Join</a></li>
+                <li><a href="/login" id="menu">Login</a></li>
+            </ul>
+        </div>
+        <div class="middle">
+            <div class="hr"></div>
+            <div class="info_art">
+                <img src="https://scontent-a-mia.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/10264619_629664303792552_997089171076589457_n.jpg?oh=175e13f2125f7a0f297d5c18922ecae7&oe=546697E6" width="60px" height="60px" class="author-image">
+                <div class="art_name">
+                    "Name of the Art"
+                </div>
+                <div class="author_name">
+                    by Guilherme Vieira Rizzo
+                </div>
+                <div class="amount">
+                    <div class="amount_dollar">
+                        $359
+                    </div>
+                    donated
+                </div>
+            </div>
+        </div>
+         <div style="height:20px;"></div>
+
+        <div class="art">
+            <img src="http://sharingdreams.url.ph/artes/Firefox_wallpaper.png" class="full_art">
+            <div class="about_donate">
+                <div class="why">
+                    About donate
+                </div>
+                <p style"text-indent: 10px;" style=" margin-top:5px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis, felis id aliquet tincidunt, urna neque venenatis metus, vel elementum nulla mauris eu dolor. And flw leks.</p>
+                <div class="donate_button">
+                    Donate $1
+                </div>
+            </div>
+            <div class="fb-like" data-href="http://sharingdreams.url.ph/artes/2014-08-30-175359.jpg" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true" style="display:inherit; margin-top:10px;"></div>
+            <a href="http://www.pinterest.com/pin/create/button/?url=http://sharingdreams.url.ph/&media=http://sharingdreams.url.ph/artes/2014-08-30-175540.jpg&description=Look this art made by LeoFelipe! I loved it!!" data-pin-do="buttonPin" style="margin-top:-325px; margin-left:420px; position:absolute;">
+                <img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" />
+            </a>
+        </div>
+
+        <div class="more_of">
+            <br><br>More of Guilherme Vieira Rizzo<br>
+            <img src="http://sharingdreams.url.ph/artes/singed.jpg" class="more_of_art">
+            <img src="http://sharingdreams.url.ph/artes/Nidalee-Classic.jpg" class="more_of_art">
+            <img src="http://sharingdreams.url.ph/artes/Screenshot%20from%202014-07-19%2021:03:32.png" class="more_of_art">
+        </div>
+
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+
+    </body>
+
+</html>
