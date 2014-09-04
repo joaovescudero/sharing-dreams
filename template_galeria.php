a4be13b8be2af0447a1198c99b28310c3ae5aed8
Guilherme Vieira Rizzo <guivr2011@gmail.com>
2014-09-04 18:39:45 -0300

    Update template_galeria.php

--- template_galeria.php
+++ template_galeria.php
@@ -23,6 +23,29 @@
                     <li align="center" style="display: inline-block; width: 300px;">
                         <div class="view view-fifth">
                             <img src="artes/<?php echo $arte['nome']; ?>" style="width:300px; height: 200px;"/>
+                            <div class="mask">
+                                <center>
+                                    <br>Did you like it?
+                                    <br>	<a href="#" style="margin-top:15px;" class="donate">
+    									See more
+    								</a>
+                                    <div style="height:5px;"></div>
+                                    <div class="fb-like" data-href="http://sharingdreams.url.ph/artes/<?php echo $arte['nome']; ?>" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
+                                        <a href="http://www.pinterest.com/pin/create/button/?url=http://sharingdreams.url.ph/&media=http://sharingdreams.url.ph/artes/<?php echo $arte['nome']; ?>&description=Look this art made by <?php echo $usuario_artista; ?>! I loved it!!" data-pin-do="buttonPin">
+        <img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" />
+    </a>
+                                </center>
+
+                                
+                                    
+                                <?php if (isset($foto_artista)) : ?>    
+                                    <a href='/conta.php?user=<?php echo $usuario_artista; ?>'><img src="fotos/<?php echo $foto_artista['nome'] ?>" class="img-author" style="position:absolute; width:41px; height:41px;"></a>
+    							<?php else : ?>
+                                    <a href='conta.php?user=<?php echo $usuario_artista; ?>'><img src="img/sem-foto.png" class="img-author" style="position:absolute; width:41px; height:41px;"></a>
+                                <?php endif ?>
+                                <p class="name-art"  style="position:absolute;">"<?php echo $arte['nome_arte']; ?>"</p>
+    							<a href='/conta.php?user=<?php echo $usuario_artista; ?>'><p class="name-author"  style="position:absolute;"><?php echo $nome_artista; ?></p></a>
+                            </div>
                         </div>
                     </li>
                 <?php endwhile; ?>
@@ -56,3 +79,4 @@
 
             <br>
         </div>
+
