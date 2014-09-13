304c794ad75abfec48210b9d63777bc5a1c8dc06
Guilherme Vieira Rizzo <guivr2011@gmail.com>
2014-09-13 19:29:03 -0300

    Update art.php

--- art.php
+++ art.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset='UTF-8'>
         <title>Sharing Dreams</title>
-        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="http://sharingdreams.url.ph/css/index.css">
         <link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
     </head>
     
@@ -19,14 +19,13 @@
             }(document, 'script', 'facebook-jssdk'));</script>
         <!-- OK. !-->
 
-<<<<<<< HEAD
         <div class="middle">
             <div class="hr"></div>
             <div class="info_art">
             <?php if (isset($foto_artista)) : ?>    
                     <a href='/conta.php?user=<?php echo $usuario_artista; ?>'><img src="fotos/<?php echo $foto_artista['nome'] ?>" width="60px" height="60px" class="author-image"></a>
                 <?php else : ?>
-                    <a href='/conta.php?user=<?php echo $usuario_artista; ?>'><img src="img/sem-foto.png" width="60px" height="60px" class="author-image"></a>
+                    <a href='/conta.php?user=<?php echo $usuario_artista; ?>'><img src="http://sharingdreams.url.ph/img/sem-foto.png" width="60px" height="60px" class="author-image"></a>
             <?php endif; ?>
                 <div class="art_name">
                     "<?php echo $arte['nome_arte']; ?>"
@@ -37,31 +36,6 @@
                 <div class="amount">
                     <div class="amount_dollar">
                         $0
-=======
-        <div class="top">
-            <div class="logo">
-                <a href='/'><img src="img/logo.png"></a>
-            </div>
-            <ul>
-                <li>About</li>
-                <li><a href="/join" id="menu">Join</a></li>
-                <li><a href="/login" id="menu">Login</a></li>
-            </ul>
-        </div>
-        <div class="middle">
-            <div class="hr"></div>
-            <div class="info_art">
-                <img src="https://scontent-a-mia.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/10264619_629664303792552_997089171076589457_n.jpg?oh=175e13f2125f7a0f297d5c18922ecae7&oe=546697E6" width="60px" height="60px" class="author-image">
-                <div class="art_name">
-                    "Name of the Art"
-                </div>
-                <div class="author_name">
-                    by Guilherme Vieira Rizzo
-                </div>
-                <div class="amount">
-                    <div class="amount_dollar">
-                        $359
->>>>>>> 9b5a830a71b81205464b0f5eae604af88cecaa51
                     </div>
                     donated
                 </div>
@@ -70,11 +44,7 @@
          <div style="height:20px;"></div>
 
         <div class="art">
-<<<<<<< HEAD
-            <img src="artes/<?php echo $arte['nome']; ?>" class="full_art">
-=======
-            <img src="http://sharingdreams.url.ph/artes/Firefox_wallpaper.png" class="full_art">
->>>>>>> 9b5a830a71b81205464b0f5eae604af88cecaa51
+            <img src="http://sharingdreams.url.ph/artes/<?php echo $arte['nome']; ?>" class="full_art">
             <div class="about_donate">
                 <div class="why">
                     About donate
@@ -84,30 +54,21 @@
                     Donate $1
                 </div>
             </div>
-            <div class="fb-like" data-href="http://sharingdreams.url.ph/artes/2014-08-30-175359.jpg" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true" style="display:inherit; margin-top:10px;"></div>
-            <a href="http://www.pinterest.com/pin/create/button/?url=http://sharingdreams.url.ph/&media=http://sharingdreams.url.ph/artes/2014-08-30-175540.jpg&description=Look this art made by LeoFelipe! I loved it!!" data-pin-do="buttonPin" style="margin-top:-325px; margin-left:420px; position:absolute;">
+            <div class="fb-like" data-href="http://sharingdreams.url.ph/artes/<?php echo $arte['nome']; ?>" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true" style="display:inherit; margin-top:10px;"></div>
+            <a href="http://www.pinterest.com/pin/create/button/?url=http://sharingdreams.url.ph/&media=http://sharingdreams.url.ph/artes/<?php echo $arte['nome']; ?>&description=Look this art made by <?php echo $nome_artista; ?>! I loved it!!" data-pin-do="buttonPin" style="margin-top:-325px; margin-left:420px; position:absolute;">
                 <img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" />
             </a>
         </div>
 
         <div class="more_of">
-<<<<<<< HEAD
             <br><br>More of <?php echo $nome_artista; ?><br>
             <?php foreach ($artes_artista as $arte_artista) : ?>
-                <img src="artes/<?php echo $arte_artista['nome']; ?>" class="more_of_art">
+                <img src="http://sharingdreams.url.ph/artes/<?php echo $arte_artista['nome']; ?>" class="more_of_art">
             <?php endforeach; ?>
         </div>
 
         <div style="height:40px;"></div>
 
-=======
-            <br><br>More of Guilherme Vieira Rizzo<br>
-            <img src="http://sharingdreams.url.ph/artes/singed.jpg" class="more_of_art">
-            <img src="http://sharingdreams.url.ph/artes/Nidalee-Classic.jpg" class="more_of_art">
-            <img src="http://sharingdreams.url.ph/artes/Screenshot%20from%202014-07-19%2021:03:32.png" class="more_of_art">
-        </div>
-
->>>>>>> 9b5a830a71b81205464b0f5eae604af88cecaa51
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
 
     </body>
