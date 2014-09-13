8abbc34f64da694d235cc2fd3f609192d63e837a
Guilherme Vieira Rizzo <guivr2011@gmail.com>
2014-09-13 19:26:53 -0300

    Update template_conta.php

--- template_conta.php
+++ template_conta.php
@@ -4,8 +4,8 @@
 		<meta charset='UTF-8'>
 		<title>Profile: <?php echo $usuario_perfil; ?></title>
         <link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="css/datepicker.css">
-		<script src="js/datepicker.js"></script> 
+		<link rel="stylesheet" href="http://sharingdreams.url.ph/css/datepicker.css">
+		<script src="http://sharingdreams.url.ph/js/datepicker.js"></script> 
 
 		<style>
 				.capa {
@@ -26,13 +26,13 @@
 				}
 		</style>
 
-		<link rel="stylesheet" href="css/index.css">
+		<link rel="stylesheet" href="http://sharingdreams.url.ph/css/index.css">
 	</head>
 	<body>
 
          <div class="top">
             <div class="logo">
-                <a href="http://sharingdreams.url.ph/"><img src="img/logo.png"></a>
+                <a href="http://sharingdreams.url.ph/"><img src="http://sharingdreams.url.ph/img/logo.png"></a>
             </div>
             <ul>
                 <li>About</li>
@@ -45,19 +45,19 @@
 				?>
 
 				<?php if(isset($_SESSION['usuario_logado'])) : ?>
-                	<li><a href='/editProfile' id='menu'>Settings</a>
+                	<li><a href='http://sharingdreams.url.ph/editProfile' id='menu'>Settings</a>
 					
-					<li><a href='deslogar.php' id='menu'>Logout</a>
+					<li><a href='http://sharingdreams.url.ph/deslogar.php' id='menu'>Logout</a>
 
 			   		<?php if (isset($_SESSION['foto'])) : ?>
-						<li><a href="/conta.php?user=<?php echo $_SESSION['usuario']; ?>"><img src='fotos/<?php echo $_SESSION['foto']['nome']; ?>' width='50px' height='50px' style='-webkit-border-radius:500; -moz-border-radius: 500px; border-radius: 500px; float:right; margin-top:-20px;'></a></li>
+						<li><a href="http://sharingdreams.url.ph/conta.php?user=<?php echo $_SESSION['usuario']; ?>"><img src='http://sharingdreams.url.ph/fotos/<?php echo $_SESSION['foto']['nome']; ?>' width='50px' height='50px' style='-webkit-border-radius:500; -moz-border-radius: 500px; border-radius: 500px; float:right; margin-top:-20px;'></a></li>
 					<?php else : ?>
-						<li><a href='/conta.php?user=<?php echo $_SESSION['usuario']; ?>'><img src='img/sem-foto.png' width='50px' height='50px' style='-webkit-border-radius:500; -moz-border-radius: 500px; border-radius: 500px; float:right; margin-top:-20px;'></a></li>
+						<li><a href='http://sharingdreams.url.ph/conta.php?user=<?php echo $_SESSION['usuario']; ?>'><img src='http://sharingdreams.url.ph/img/sem-foto.png' width='50px' height='50px' style='-webkit-border-radius:500; -moz-border-radius: 500px; border-radius: 500px; float:right; margin-top:-20px;'></a></li>
 					<?php endif; ?>	
 
 				<?php else : ?>
-					<li><a href="/join" id="menu">Join</a></li>
-            		<li><a href="/login" id="menu">Login</a></li>
+					<li><a href="http://sharingdreams.url.ph/join" id="menu">Join</a></li>
+            		<li><a href="http://sharingdreams.url.ph/login" id="menu">Login</a></li>
 				<?php endif; ?>
 
             </ul>
@@ -67,9 +67,9 @@
 			<center>
 				<div class="capa">
 				<?php if (isset($foto_perfil)) :?>
-					<img src='fotos/<?php echo $foto_perfil['nome']; ?>' width="150px" height="150px" style="display:ii; margin-top: 30px; -webkit-border-radius:500; -moz-border-radius: 500px; border-radius: 500px;">
+					<img src='http://sharingdreams.url.ph/fotos/<?php echo $foto_perfil['nome']; ?>' width="150px" height="150px" style="display:ii; margin-top: 30px; -webkit-border-radius:500; -moz-border-radius: 500px; border-radius: 500px;">
 				<?php else : ?>
-					<img src='img/sem-foto.png' width="150px" height="150px" style="display:ii; margin-top: 30px; -webkit-border-radius:500; -moz-border-radius: 500px; border-radius: 500px;">
+					<img src='http://sharingdreams.url.ph/img/sem-foto.png' width="150px" height="150px" style="display:ii; margin-top: 30px; -webkit-border-radius:500; -moz-border-radius: 500px; border-radius: 500px;">
 				<?php endif; ?>
 					<div class="txtg" style="margin-top:20px;"><?php echo $nome_perfil; ?></div>
 					<div class="city"><?php echo $endereco_perfil; ?></div>
@@ -97,22 +97,22 @@
 							<li align="center" style="display: inline-block; width: 300px;">
 								<div class="view view-fifth">
 
-									<img src="artes/<?php echo $arte_artista['nome']; ?>" style="width:300px; height: 200px;" />
+									<img src="http://sharingdreams.url.ph/artes/<?php echo $arte_artista['nome']; ?>" style="width:300px; height: 200px;" />
 									<div class="mask">
 										<center>
 										<br>
 											Did you like it?
 										<br>
-											<a href="/?art=<?php echo $arte_artista['id']; ?>" style="margin-top:15px;" class="donate">
+											<a href="http://sharingdreams.url.ph/?art=<?php echo $arte_artista['id']; ?>" style="margin-top:15px;" class="donate">
 												See more
 											</a>
 
 										</center>
 
 										<?php if (isset($foto_perfil)) :?>
-											<img src='fotos/<?php echo $foto_perfil['nome']; ?>' class="img-author" style="position:absolute; position:absolute; width:41px; height:41px;">
+											<img src='http://sharingdreams.url.ph/fotos/<?php echo $foto_perfil['nome']; ?>' class="img-author" style="position:absolute; position:absolute; width:41px; height:41px;">
 										<?php else : ?>
-											<img src='img/sem-foto.png' class="img-author" style="position:absolute; position:absolute; width:41px; height:41px;">
+											<img src='http://sharingdreams.url.ph/img/sem-foto.png' class="img-author" style="position:absolute; position:absolute; width:41px; height:41px;">
 										<?php endif; ?>
 										<p class="name-art" style="position:absolute;">"<?php echo $arte_artista['nome_arte']; ?>"</p>
 										<p class="name-author" style="position:absolute;"><?php echo $nome_perfil; ?></p>
@@ -126,7 +126,7 @@
 			                <?php
 
 			                    if ($pagina_atual > 1) {
-			                        echo "<div class='page-button'><a href='/conta.php?user=$user&page=".($pagina_atual - 1)."'><<</a></div>";
+			                        echo "<div class='page-button'><a href='http://sharingdreams.url.ph/conta.php?id=$id_perfil&page=".($pagina_atual - 1)."'><<</a></div>";
 			                    }
 
 			                    for($i = $inicio; $i <= $limite + 1; $i++) {
@@ -134,13 +134,13 @@
 			                            echo "<div class='page-button stroke-page'>".$pagina_atual."</div> ";
 			                        } else {
 			                            if ($i >= 1 && $i <= $numPaginas) {
-			                                echo "<div class='page-button'><a  id='num-page' href='/conta.php?user=$user&page=$i'>".$i."</a></div> ";
+			                                echo "<div class='page-button'><a  id='num-page' href='http://sharingdreams.url.ph/conta.php?id=$id_perfil&page=$i'>".$i."</a></div> ";
 			                            }
 			                        }
 			                    }
 
 			                    if ($pagina_atual < $numPaginas) {
-			                        echo "<div class='page-button'><a href='/conta.php?user=$user&page=".($pagina_atual + 1)."'>>></a></div>";
+			                        echo "<div class='page-button'><a href='http://sharingdreams.url.ph/conta.php?id=$id_perfil&page=".($pagina_atual + 1)."'>>></a></div>";
 			                    }
 			                ?>
 			            </center>
