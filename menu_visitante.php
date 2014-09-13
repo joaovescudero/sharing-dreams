5e48d5ec6c0f1463466fce457b66a17cb525f2ef
Guilherme Vieira Rizzo <guivr2011@gmail.com>
2014-09-13 19:29:31 -0300

    Update menu_visitante.php

--- menu_visitante.php
+++ menu_visitante.php
@@ -13,7 +13,7 @@ include "helper.php";
 
 <div class="top">
     <div class="logo">
-        <a href='/'><img src="img/logo.png" class="logo_img"></a>
+        <a href='/'><img src="http://sharingdreams.url.ph/img/logo.png" class="logo_img"></a>
     </div>
     <ul class="menu_list">
         <li>About</li>
@@ -26,7 +26,7 @@ include "helper.php";
         <div class="txtg" style="margin-top:55px;">Help kids around the world.
             <br>	<a href="#" style="font-size:18px;">About</a>
             <br>
-            <img src="img/about.png" class="about_img">
+            <img src="http://sharingdreams.url.ph/img/about.png" class="about_img">
         </div>
     <div class="hr" style="margin-top:15px;"></div>
 </div>
