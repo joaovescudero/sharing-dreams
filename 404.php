6db7638d98232d2940e33457427e9c7e50f534da
Guilherme Vieira Rizzo <guivr2011@gmail.com>
2014-09-13 19:28:53 -0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -2,13 +2,13 @@
     
     <head>
         <title>Sharing Dreams - 404</title>
-        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="http://sharingdreams.url.ph/css/index.css">
         <link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
     </head>
     
     <body bgcolor="#efefef">
 		<center>
-			<img src="img/404.png">
+			<img src="http://sharingdreams.url.ph/img/404.png">
 		</center>
 	</body>
 </html>
