b129d4cd896ea552b11d93d06ca424fc55192282
Guilherme Vieira Rizzo <guivr2011@gmail.com>
2014-09-13 19:29:39 -0300

    Update participar.php

--- participar.php
+++ participar.php
@@ -3,28 +3,28 @@
 	<head>
 		<meta charset='UTF-8'>
 		<title>Join- Sharing Dreams</title>
-		<link rel="stylesheet" href="css/bootstrap.css">
-		<link rel="stylesheet" href="css/index.css">
-        <link rel="stylesheet" href="css/cadastro.css">
+		<link rel="stylesheet" href="http://sharingdreams.url.ph/css/bootstrap.css">
+		<link rel="stylesheet" href="http://sharingdreams.url.ph/css/index.css">
+        <link rel="stylesheet" href="http://sharingdreams.url.ph/css/cadastro.css">
         <link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
 		<script src="http://anontime.com/vunch/js/jquery.js"></script>
 		<script src="http://anontime.com/vunch/js/jquery-ui-1.10.4.custom.min.js"></script> 
 		<script src="http://anontime.com/vunch/js/jquery-ui-1.10.4.custom.js"></script> 
 		<script src="http://anontime.com/vunch/js/bootstrap.js"></script>
 		<script type="text/javascript" src="http://anontime.com/vunch/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" href="css/datepicker.css">
-		<script src="js/datepicker.js"></script>  
+		<link rel="stylesheet" href="http://sharingdreams.url.ph/css/datepicker.css">
+		<script src="http://sharingdreams.url.ph/js/datepicker.js"></script>  
 	</head>
 	<body>
 
 		<div class="top">
             <div class="logo">
-                <a href="http://sharingdreams.url.ph"><img src="img/logo.png"></a>
+                <a href="http://sharingdreams.url.ph"><img src="http://sharingdreams.url.ph/img/logo.png"></a>
             </div>
             <ul>
                 <li>About</li>
                 <li>Join</li>
-                <li><a href="/login" id="menu">Login</a></li>
+                <li><a href="http://sharingdreams.url.ph/login" id="menu">Login</a></li>
             </ul>
         </div>
 		<div class="hr" style="margin-top:15px;"></div>
